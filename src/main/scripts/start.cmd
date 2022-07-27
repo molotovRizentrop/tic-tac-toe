a3e3ef34a8ff43db782e1a5ee2dc232ecf90f779
kashtan <bassanddub.co@gmail.com>
2022-07-27 21:01:51 +0300

    Add start script for windows/unix OS

---
+++ src/main/scripts/start.cmd
@@ -0,0 +1,7 @@
+@rem
+@rem script for windows OS
+@rem
+
+@echo off
+java -jar tic-tac-toe-1.0-SNAPSHOT.jar
+pause
---
+++ src/main/scripts/start.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+#
+#script for unix OS
+#
+java -jar tic-tac-toe-1.0-SNAPSHOT.jar
