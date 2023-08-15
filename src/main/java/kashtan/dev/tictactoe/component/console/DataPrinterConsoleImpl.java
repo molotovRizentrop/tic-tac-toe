4801c6c6274827bcca7e8db1b3912174f594d7a4
kashtan <bassanddub.co@gmail.com>
2023-08-15 03:43:47 +0300

    Implementation of the console user interface classes

---
+++ src/main/java/kashtan/dev/tictactoe/component/console/DataPrinterConsoleImpl.java
@@ -0,0 +1,55 @@
+package kashtan.dev.tictactoe.component.console;
+
+import kashtan.dev.tictactoe.component.CellNumberConverter;
+import kashtan.dev.tictactoe.component.DataPrinter;
+import kashtan.dev.tictactoe.component.DataPrinterImpl;
+import kashtan.dev.tictactoe.model.Cell;
+import kashtan.dev.tictactoe.model.GameTable;
+
+/**
+ * @author:kashtan
+ * @email:bassanddub.co@gmail.com
+ **/
+public class DataPrinterConsoleImpl implements DataPrinter {
+    private final CellNumberConverter cellNumberConverter;
+
+    public DataPrinterConsoleImpl(final CellNumberConverter cellNumberConverter) {
+        this.cellNumberConverter = cellNumberConverter;
+    }
+
+    @Override
+    public void printInfoMessage(final String text) {
+        System.out.println(text);
+    }
+
+    @Override
+    public void printErrorMessage(final String text) {
+        System.err.println(text);
+    }
+
+    @Override
+    public void printMappingTable() {
+        print((i, j) -> String.valueOf(cellNumberConverter.toNumber(new Cell(i, j))));
+    }
+
+    @Override
+    public void printGameTable(final GameTable gameTable) {
+        print((i, j) -> String.valueOf(gameTable.getSign(new Cell(i, j))));
+    }
+
+    public void print(final Lambda lambda) {
+        for (int i = 0; i < 3; i++) {
+            System.out.println("-------------");
+            for (int j = 0; j < 3; j++) {
+                System.out.print("| " + lambda.getValue(i, j) + " ");
+            }
+            System.out.println("|");
+        }
+        System.out.println("-------------");
+    }
+
+    @FunctionalInterface
+    private interface Lambda {
+        String getValue(int i, int j);
+    }
+}
---
+++ src/main/java/kashtan/dev/tictactoe/component/console/UserInputReaderConsoleImpl.java
@@ -0,0 +1,37 @@
+package kashtan.dev.tictactoe.component.console;
+
+import kashtan.dev.tictactoe.component.CellNumberConverter;
+import kashtan.dev.tictactoe.component.DataPrinter;
+import kashtan.dev.tictactoe.component.UserInputReader;
+import kashtan.dev.tictactoe.model.Cell;
+
+import java.util.Scanner;
+
+/**
+ * @author:kashtan
+ * @email:bassanddub.co@gmail.com
+ **/
+public class UserInputReaderConsoleImpl implements UserInputReader {
+
+    private final CellNumberConverter cellNumberConverter;
+    private final DataPrinter dataPrinter;
+
+    public UserInputReaderConsoleImpl(final CellNumberConverter cellNumberConverter, final DataPrinter dataPrinter) {
+        this.cellNumberConverter = cellNumberConverter;
+        this.dataPrinter = dataPrinter;
+    }
+
+    @Override
+    public Cell getUserInput() {
+        while (true) {
+            dataPrinter.printInfoMessage("Please type number between 1 and 9:");
+            final String userInput = new Scanner(System.in).nextLine();
+            if (userInput.length() == 1) {
+                final char ch = userInput.charAt(0);
+                if (ch >= '1' && ch <= '9') {
+                    return cellNumberConverter.getCell(ch);
+                }
+            }
+        }
+    }
+}
